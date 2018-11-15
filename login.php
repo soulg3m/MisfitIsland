24ead8ef87b3efb009c0e068e3f0ee5c8e620b09
alexisd7 <45074741+alexisd7@users.noreply.github.com>
2018-11-15 11:34:50 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Misfit Island</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/styles.css">
+        
+        <div class="header">
+            <div class="nav">
+                <ul id="nav">
+                    <li><a href="home.php">Logo</a></li>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="events.php">Events</a></li>
+                    <li style="float:right"><a href="login.php">Log In</a></li>
+                    <li style="float:right"><a href="cart.php">Shopping Cart</a></li>
+                </ul>
+            </div>
+        </div>
+    </head>
+    <body>
+        Login
+        <div class="hero">
+            <h1 class="center">Hero Image</h1>
+        </div>
+        <div class="deals">
+            <div class="deals-grid-container">
+                <div class="deals-grid-item"></div>
+                <div class="deals-grid-item"></div>
+                <div class="deals-grid-item"></div>
+                <div class="deals-grid-item"></div>
+                <div class="deals-grid-item"></div>
+                <div class="deals-grid-item"></div>
+            </div>
+        </div>
+        <div class="app">
+            <p class="center">We have an app!</p>
+        </div>
+        <div class="footer">
+            <div class="foot-grid-container">
+                <div class="foot-grid-item">Header Links</div>
+                <div class="foot-grid-item">Social Media</div>
+                <div class="foot-grid-item">Newsletter</div>
+            </div>
+        </div>
+    </body>
+    <footer>
+        
+    </footer>
+</html>
---
+++ shop.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Misfit Island</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/styles.css">
+        
+        <div class="header">
+            <div class="nav">
+                <ul id="nav">
+                    <li><a href="home.php">Logo</a></li>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="events.php">Events</a></li>
+                    <li style="float:right"><a href="login.php">Log In</a></li>
+                    <li style="float:right"><a href="cart.php">Shopping Cart</a></li>
+                </ul>
+            </div>
+        </div>
+    </head>
+    <body>
+        Shop
+        <div class="shop-grid-container">
+            <div class="shop-grid-item">Filter</div>
+            <div class="shop-grid-item">Main Content</div>
+        </div>
+        <div class="footer">
+            <div class="foot-grid-container">
+                <div class="foot-grid-item">Header Links</div>
+                <div class="foot-grid-item">Social Media</div>
+                <div class="foot-grid-item">Newsletter</div>
+            </div>
+        </div>
+    </body>
+    <footer>
+        
+    </footer>
+</html>
