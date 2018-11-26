9e63183e23b8a45a15ad6c158914aa65d5a3fc0d
aziamendez <37088313+aziamendez@users.noreply.github.com>
2018-11-26 10:44:32 -0500

    Add files via upload

--- home.php
+++ home.php
@@ -5,39 +5,53 @@
         <title>Misfit Island</title>
         <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/styles.css">
-        
-        <div class="header">
-            <div class="nav">
-                <ul id="nav">
-                    <li><a href="home.php">Logo</a></li>
-                    <li><a href="home.php">Home</a></li>
-                    <li><a href="shop.php">Shop</a></li>
-                    <li><a href="about.php">About Us</a></li>
-                    <li><a href="events.php">Events</a></li>
-                    <li style="float:right"><a href="login.php">Log In</a></li>
-                    <li style="float:right"><a href="cart.php">Shopping Cart</a></li>
-                </ul>
-            </div>
-        </div>
     </head>
     <body>
-        Home
+
+      <div class="header">
+          <div class="nav">
+              <ul>
+                  <li><a href="home.php">Logo</a></li>
+                  <li><a class="active" href="home.php">Home</a></li>
+                  <li><a href="shop.php">Shop</a></li>
+                  <li><a href="about.php">About Us</a></li>
+                  <li><a href="events.php">Events</a></li>
+                  <li style="float:right"><a href="login.php">Log In</a></li>
+                  <li style="float:right"><a href="cart.php">Shopping Cart</a></li>
+              </ul>
+          </div>
+      </div>
+
         <div class="hero">
             <h1 class="center">Hero Image</h1>
         </div>
-        <div class="deals">
-            <div class="deals-grid-container">
-                <div class="deals-grid-item"></div>
-                <div class="deals-grid-item"></div>
-                <div class="deals-grid-item"></div>
-                <div class="deals-grid-item"></div>
-                <div class="deals-grid-item"></div>
-                <div class="deals-grid-item"></div>
-            </div>
+
+
+        <div class="container">
+          <h1 class="center">Deals</h1>
+
+          <?php
+
+            $dir = glob('images/{*.jpg,*.png}',GLOB_BRACE);
+
+            foreach($dir as $key=> $value)
+            {
+              ?>
+              <div class="photo">
+                <img src="<?php echo $value;?>" alt="<?php echo $value;?>">
+              </div>
+              <?php
+            }
+            ?>
+
+          <div class="left-div">
+            WE GOT A NEW APPLEFT LEFT LEFT LEFT LEFT LEFT LEFT LEFT LEFT
+          </div>
+          <div class="right-div">
+            STUFF ABOUT THE APP LIKE WE SAW ON MACY'S RIGHT RIGHT RIGHT RIGHT RIGHT RIGHT RIGTH RIGHT
+          </div>
         </div>
-        <div class="app">
-            <p class="center">We have an app!</p>
-        </div>
+
         <div class="footer">
             <div class="foot-grid-container">
                 <div class="foot-grid-item">Header Links</div>
@@ -45,8 +59,7 @@
                 <div class="foot-grid-item">Newsletter</div>
             </div>
         </div>
+
     </body>
-    <footer>
-        
-    </footer>
+
 </html>
